34727749f19742b08c9fde68ff5fa10c9cf43a7d
Ujjwal Tiwari <pyramidfashionworld@gmail.com>
2025-11-26 20:14:10 +0530

    Fix checkout route

--- app/api/checkout/page.tsx
+++
@@ -1,5 +0,0 @@
-import CheckoutFlow from '@/components/CheckoutFlow';
-
-export default function CheckoutPage() {
-  return <CheckoutFlow />;
-}
--- page.js
+++
@@ -1,7 +0,0 @@
-// <model-viewer
-//   src="/models/baby_clothes.glb"
-//   alt="3D Baby Clothes"
-//   auto-rotate
-//   camera-controls
-//   style={{ width: '300px', height: '300px' }}
-// ></model-viewer>
