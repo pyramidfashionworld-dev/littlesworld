01ab4fb77b7e2374712854a46dbc90c8e0343476
Ujjwal Tiwari <pyramidfashionworld@gmail.com>
2025-12-04 19:52:09 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,27 @@
 module.exports = {
-  images: {
-    domains: ['littlesworld.co.in'],
-    formats: ['image/webp', 'image/avif'],
+  // ... existing config
+  webpackDevMiddleware: (config) => {
+    return config;
   },
-  compress: true,
-  swcMinify: true,
-  experimental: {
-    isrMemoryCacheSize: 50 * 1024 * 1024,
+  headers: async () => {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          }
+        ]
+      }
+    ];
   }
 }
